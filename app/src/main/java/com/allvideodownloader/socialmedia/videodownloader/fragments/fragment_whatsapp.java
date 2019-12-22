3d027bb79244bd17336afb83a8ecf0c7a1d23183
Mathan.S <capturesmk@gmail.com>
2019-12-22 20:25:40 +0530

    Whatsapp Fragment begins :>

--- app/src/main/java/com/allvideodownloader/socialmedia/videodownloader/fragments/fragment_whatsapp.java
+++ app/src/main/java/com/allvideodownloader/socialmedia/videodownloader/fragments/fragment_whatsapp.java
@@ -1,34 +1,24 @@
 package com.allvideodownloader.socialmedia.videodownloader.fragments;
 
-
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.allvideodownloader.socialmedia.videodownloader.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class fragment_whatsapp extends Fragment {
-
 
     public fragment_whatsapp() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View rootView = inflater.inflate(R.layout.fragment_whatsapp, container, false);
+        return rootView;
     }
-
 }
